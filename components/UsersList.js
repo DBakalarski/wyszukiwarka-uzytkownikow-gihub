ad3edd2ce9a1f9d5669060f0153c88ddaac801c0
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-07-05 15:56:13 +0200

    first commit

---
+++ components/UsersList.js
@@ -0,0 +1,13 @@
+class UserList extends React.Component {
+	get users() {
+		return this.props.users.map(user => <User key={user.id} user={user}/>);
+	}
+
+	render() {
+		return (
+			<div>
+				{this.users}
+			</div>
+		);
+	}
+}
